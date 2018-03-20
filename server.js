f3016844f89fd0309a502bf41270f5dbcca0bc0a
Pablo Velez <Pablovelez@AL-COL-02313.local>
2018-03-19 21:06:13 -0500

    removing commented code

--- server.js
+++ server.js
@@ -13,13 +13,7 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-//app.use(express.static(path.join(__dirname, 'dist')));
-
 app.use('/', api);
-
-//app.get('*', (req, res) => {
-//    res.sendFile(path.join(__dirname, 'dist/index.html'));
-//});
 
 //set port
 const port = process.env.PORT || 3000;
@@ -27,7 +21,4 @@ app.set('port', port);
 
 const server = http.createServer(app);
 
-
 server.listen(port, () => console.log(`Running on localhost:${port}`));
-
-//mlab info: mongodb://<dbuser>:<dbpassword>@ds247688.mlab.com:47688/blogapp
